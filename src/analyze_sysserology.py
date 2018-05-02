c68a4af2d9d0ff0fdb7d79bc69352adde69e557d
Laura Hughes <flaneuse@users.noreply.github.com>
2018-05-02 10:58:36 -0700

    update

--- src/analyze_sysserology.py
+++ src/analyze_sysserology.py
@@ -18,18 +18,22 @@ from sysserology_helpers import read_plates, create_dirs
 # [0] Define the locations of the files -------------------------------------------------------------------
 wd = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/'
 
+# lookup table containing the sample ids + locations on the plate
 platefile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNP_PlateLayout.xlsx'
+
+# Export from FlowJo containing the processed fluorescence counts
+fluorfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNP_data from FlowJo.xlsx'
+
+# zipped file from FlowJo containing the raw data files + gating info + metadata
+fcsfile = 'ADNP_Tulane.acs'
 
 # [1] Read in the plate metadata file ---------------------------------------------------------------------
 plate_dict, expt_ids = read_plates(platefile)
-
-for x,y in expt_ids.items():
-    print(x)
-    print(y)
-
 
 # [2] Create dirs + logfile -------------------------------------------------------------------------------
 # Returns a dict containing all the necessary paths for the experiment
 expt_dict = create_dirs(expt_ids, wd)
 
 expt_dict
+
+# [3] Pull out metadata and rename files ------------------------------------------------------------------
