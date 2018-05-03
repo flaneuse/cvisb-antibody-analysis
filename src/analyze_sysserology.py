df8209e55e3f045ed1a60f746929a4c736bca4ba
Laura Hughes <flaneuse@users.noreply.github.com>
2018-05-03 15:58:09 -0700

    add in all expts

--- src/analyze_sysserology.py
+++ src/analyze_sysserology.py
@@ -15,7 +15,11 @@ import os
 os.chdir('/Users/laurahughes/GitHub/cvisb-antibody-analysis/src')
 from sysserology_helpers import read_plates, create_dirs
 from metadata import getmd_renamefiles, unzip_acs
+
+from adcd import ADCD
+from adcp import ADCP
 from adnp import ADNP
+from nkd import NKD
 
 # TODO: relative paths
 # TODO: figure more convenient place for these
@@ -24,30 +28,38 @@ wd = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/'
 
 # lookup table containing the sample ids + locations on the plate
 platefile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNP_PlateLayout.xlsx'
+# platefile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/NKD_PlateLayout.xlsx'
+# # platefile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCD_PlateLayout.xlsx'
+# platefile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCP_PlateLayout.xlsx'
+
 
 # Export from FlowJo containing the processed fluorescence counts
 fluorfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNP_data from FlowJo.xlsx'
+# fluorfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNKD_data from FlowJo.xlsx'
+# # fluorfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCD_data from FlowJo.xlsx'
+# fluorfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCP_data_FlowJo.xlsx'
 
 # zipped file from FlowJo containing the raw data files + gating info + metadata
 fcsfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADNP_Tulane.acs'
+# fcsfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/NKD_Tulane analysis.acs'
+# fcsfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCD_Tulane.acs'
+# fcsfile = '/Users/laurahughes/GitHub/cvisb-antibody-analysis/example_data/ADCP_Tulane.acs'
 
 # [1] Read in the plate metadata file ---------------------------------------------------------------------
 plates, expt_ids = read_plates(platefile)
+plates
 
 # [2] Create dirs + logfile -------------------------------------------------------------------------------
 # Returns a dict containing all the necessary paths for the experiment
 expt_dict = create_dirs(expt_ids, wd)
 
-expt_dict['BMGEXP568']
+
+
 
 # [3] Pull out metadata and rename files ------------------------------------------------------------------
 getmd_renamefiles(plates, expt_dict, fcsfile, platefile, fluorfile)
 
-for expt_id, expt_dirs  in expt_dict.items():
-    print(expt_id)
-    print(expt_dirs)
-
 # [4] Call experiment calculation ------------------------------------------------------------------------
-expt = ADNP(fluorfile, plates)
-
-expt.summary
+expt = ADNP(fluorfile, plates, expt_dict)
+# expt = NKD(fluorfile, plates, expt_dict)
+# expt = ADCD(fluorfile, plates, expt_dict)
