761ffecd761a7d94fe5a731ed65347e195049793
Laura Hughes <flaneuse@users.noreply.github.com>
2018-05-03 15:57:48 -0700

    update

--- src/SysSerologyExpt.py
+++ src/SysSerologyExpt.py
@@ -12,11 +12,12 @@ import re
 class SysSerologyExpt:
     scale_factor = 1e4
 
-    def __init__(self, fluorfile, plate_dict, expt_id, expt_type):
+    def __init__(self, fluorfile, plate_dict, expt_dict):
         # import data
         self.df = self.read_fluor(fluorfile)
-        self.expt_type = expt_type
-        self.expt_id = expt_id
+        self.expt_type = self._get_expttypes(expt_dict)
+        self.expt_id = "-".join(expt_dict.keys()) # if multiple, concat together
+        self.savedirs = self._get_exptdirs(expt_dict)
 
         # merge samples to their IDs
         self._join_sampleids(plate_dict)
@@ -28,7 +29,8 @@ class SysSerologyExpt:
         self.calc_mean()
 
         # Save
-        self.export_data(self.expt_type, self.expt_id)
+        self.export_data()
+
 
 
 
@@ -56,12 +58,14 @@ class SysSerologyExpt:
     # TODO: better error if no plate number
     # TODO: self, + pandas, re
     def _get_platenum(self, sheetname):
-        plate_num = [int(s) for s in sheetname.split() if s.isdigit()]
+        plate_num = re.findall(r'\d+', sheetname)
 
         try:
-            return plate_num[0]
+            return int(plate_num[0])
         except:
-            write_logfile("Unknown plate number in input file {fluorfile}. Please label worksheets as 'Plate 1', 'Plate 2', ...")
+            pass
+            # TODO: fix
+            # logfile.write("Unknown plate number in input file {fluorfile}. Please label worksheets as 'Plate 1', 'Plate 2', ...")
 
     def _clean_fluor(self, df, plate_num):
         # remove the mean / SD rows
@@ -76,6 +80,8 @@ class SysSerologyExpt:
         # Pull out the well ID for each sample from the filename using regular expression matching
         # Pulls out the first capitalized letter followed by 1-2 digits
         df['well'] = df.filename.apply(lambda x: re.search(r'[A-Z]\d{1,2}', x).group(0))
+        df['row'] = df.well.apply(lambda x: re.search(r'[A-Z]', x).group(0)) # pull out the row letter
+        df['col'] = df.well.apply(lambda x: int(re.search(r'\d{1,2}', x).group(0))) # pull out the row letter
 
         return df
 
@@ -87,17 +93,30 @@ class SysSerologyExpt:
 
         return df
 
-    # [2] Join data with their sample IDs ---------------------------------------------------------------------
+# [2] Pull out expt metadata for saving -------------------------------------------------------------------
+    def _get_expttypes(self, expt_dict):
+        arr = []
+        for expt in expt_dict.values():
+            arr.append(expt['type'])
+        return "-".join(set(arr))
+
+    def _get_exptdirs(self, expt_dict):
+        arr = []
+        for expt in expt_dict.values():
+            arr.append(expt['exptdir'])
+        return set(arr)
+
+    # [3] Join data with their sample IDs ---------------------------------------------------------------------
     def _join_sampleids(self, plate_dict):
         import pandas as pd
 
         # TODO: check all columns exist
         self.df = pd.merge(self.df, plate_dict, how='left', on = ['plate', 'well'])
 
-# [3] Aggregate stats -------------------------------------------------------------------------------------
+# [4] Aggregate stats -------------------------------------------------------------------------------------
 # TODO: check merge has been run.
     def calc_mean(self):
-        grouping_cols = ['plate', 'sample_id', 'sample_type', 'experiment', 'expt_id']
+        grouping_cols = ['plate', 'sample_id', 'sample_type', 'experiment', 'expt_id', 'fluor_score_type']
 
         self.df['sample_mean'] = self.df.groupby(grouping_cols).fluor_score.transform('mean')
         self.df['sample_std'] = self.df.groupby(grouping_cols).fluor_score.transform('std')
@@ -106,7 +125,7 @@ class SysSerologyExpt:
         # collapsed version
         self.summary = self.df.groupby(grouping_cols).agg({'fluor_score': ['mean', 'std', 'count', self._get_indivs]})
 
-        self.summary.rename(columns = {'_get_indivs': 'indiv_scores', 'count': 'num_obs'})
+        self.summary.rename(columns = {'_get_indivs': 'indiv_scores', 'count': 'num_obs'}, inplace = True)
 
     def _get_indivs(self, arr):
         if(len(arr) == 1):
@@ -114,12 +133,13 @@ class SysSerologyExpt:
         else:
             return tuple(arr)
 
-# [4] Save data -------------------------------------------------------------------------------------------
-    def export_data(self, expt_type, expt_id, excel = True):
-        filename = f'sysserology-{expt_type}-{expt_id}'
-        if (excel):
-            self.df.to_excel(filename + ".xlsx")
-            self.summary.to_excel(filename + "_summary.xlsx")
-        else:
-            self.df.to_csv(filename + ".csv")
-            self.summary.to_csv(filename + "_summary.csv")
+# [5] Save data -------------------------------------------------------------------------------------------
+    def export_data(self, excel = True):
+        for savedir in self.savedirs:
+            filename = f'{savedir}sysserology-{self.expt_type}-{self.expt_id}'
+            if (excel):
+                self.df.to_excel(filename + ".xlsx")
+                self.summary.to_excel(filename + "_summary.xlsx")
+            else:
+                self.df.to_csv(filename + ".csv")
+                self.summary.to_csv(filename + "_summary.csv")
